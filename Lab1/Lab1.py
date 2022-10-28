fd6f17ed07811799aac1d9aaf768838cb49078a5
Georgy <56767741+pluxaaa@users.noreply.github.com>
2022-10-28 15:57:04 +0300

    Add files via upload

---
+++ Lab1/Lab1.py
@@ -0,0 +1,69 @@
+import math
+import sys
+
+def CoefInput(prompt):
+    while True:
+        print(prompt)
+        try:
+            coef = float(input())
+
+        except ValueError:
+            print("Ошибка ввода; введите число\n")
+
+        else:
+            print("Вы ввели: ", coef)
+            break
+    return coef
+
+def GetRoots(a, b, c):
+    result = []
+    D = b * b - 4 * a * c
+    if D >= 0:
+        D = math.sqrt(D)
+        if D == 0:
+            t = -b / (2 * a)
+            if t >= 0:
+                result.append(math.sqrt(t))
+                result.append(-1 * math.sqrt(t))
+                return result
+            else:
+                print("Нет действительных корней")
+                quit()
+
+
+        if D > 0:
+            t1 = (-b + D) / (2 * a)
+            t2 = (-b - D) / (2 * a)
+            if not ((t1 < 0) or (t2 < 0)):
+                result.append(math.sqrt(t1))
+                result.append(math.sqrt(t2))
+                result.append(-1 * math.sqrt(t1))
+                result.append(-1 * math.sqrt(t2))
+                return result
+            else:
+                print("Нет действительных корней")
+                quit()
+
+    else:
+        print("Нет действительных корней")
+        quit()
+
+def main():
+    while True:
+        a = CoefInput('Введите коэффициент  A')
+        if a != 0:
+            break
+        else:
+            print("Коэффициент A не должен равняться 0\n")
+
+    b = CoefInput('\nВведите коэффициент  B')
+    c = CoefInput('\nВведите коэффициент  C')
+
+    roots = GetRoots(a, b, c)
+
+    print("\nОтвет: ")
+    for i in range(len(roots)):
+        print(i+1, "-й корень:", roots[i])
+
+if __name__ == "__main__":
+    main()
